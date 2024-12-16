348b6b8084ee1b9615ab75fed08404e10bc5a8db
rebio2 <javiersanchezocana4@gmail.com>
2024-12-16 11:01:19 +0100

    BD_PDO_Paginacion

---
+++ test-paginacion-PDO-vo/test-paginacion-PDO-vo/app/config.php
@@ -0,0 +1,13 @@
+<?php 
+define('SERVER_DB','localhost');
+define('DB_USER','root');
+define('DB_PASSWD','');
+define('DATABASE','Clientes');
+
+// $conn = new mysqli($SERVER_DB, $DB_USER, $DB_PASSWD, $DATABASE);
+
+// Verificar la conexión
+// if ($conn->connect_error) {
+//     die("Conexión fallida: " . $conn->connect_error);
+// }
+?>
